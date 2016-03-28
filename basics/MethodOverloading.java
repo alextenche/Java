9ee45147c147adcb766ad6fcb0d0663ed59edb65
Alex Tenche <alex.tenche@gmail.com>
2016-03-29 00:11:11 +0300

    more java training

--- basics/MethodOverloading.java
+++ basics/MethodOverloading.java
@@ -1,33 +1,33 @@
 
-public class Main {
+public class MethodOverloading {
 
 	public static void main(String[] args) {
-		
+
 		int value1 = 5;
 		int value2 = 10;
 		int value3 = 15;
-		
+
 		int result = addValues(value1, value2, value3);
 		System.out.println("The result is: " + result);
-		
+
 		String string1 = "10";
 		String string2 = "25";
 		int result2 = addValues(string1,string2);
 		System.out.println("The result2 is: " + result2);
 	}
-	
-	
+
+
 	// two ints
 	private static int addValues(int int1, int int2){
 		return int1+int2;
 	}
 
-	
+
 	// three ints
 	private static int addValues(int int1, int int2, int int3){
 		return int1 + int2 + int3;
 	}
-	
+
 	// two strings
 	private static int addValues(String val1, String val2){
 		int value1 = Integer.parseInt(val1);
