a91a051d196066d4d68cf246db63a3d14c8a1f58
Alex Tenche <alex.tenche@gmail.com>
2016-04-13 22:55:24 +0300

    finished app, still some problem with time animation

--- pomodoro/src/com/teamtreehouse/pomodoro/controllers/Home.java
+++ pomodoro/src/com/teamtreehouse/pomodoro/controllers/Home.java
@@ -2,6 +2,7 @@ package com.teamtreehouse.pomodoro.controllers;
 
 import com.teamtreehouse.pomodoro.model.Attempt;
 import com.teamtreehouse.pomodoro.model.AttemptKind;
+import javafx.animation.Animation;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.beans.property.SimpleStringProperty;
@@ -9,14 +10,17 @@ import javafx.beans.property.StringProperty;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
 import javafx.scene.layout.VBox;
+import javafx.scene.media.AudioClip;
 import javafx.util.Duration;
 
 public class Home {
     @FXML private VBox container;
-
     @FXML private Label title;
+    @FXML private TextArea message;
 
+    private final AudioClip mApplause;
     private Attempt mCurrentAttempt;
     private StringProperty mTimerText;
     private Timeline mTimeline;
@@ -24,6 +28,7 @@ public class Home {
     public Home() {
         mTimerText = new SimpleStringProperty();
         setTimerText(0);
+        mApplause = new AudioClip(getClass().getResource("/sounds/applause.mp3").toExternalForm());
     }
 
     public String getTimerText() {
@@ -45,17 +50,46 @@ public class Home {
     }
 
     private void prepareAttempt(AttemptKind kind) {
-        clearAttemptStyles();
+        reset();
         mCurrentAttempt = new Attempt(kind, "");
         addAttemptStyle(kind);
         title.setText(kind.getDisplayName());
         setTimerText(mCurrentAttempt.getRemainingSeconds());
+        //TODO this is creating multiple timelines - need to fix this
         mTimeline = new Timeline();
         mTimeline.setCycleCount(kind.getTotalSeconds());
         mTimeline.getKeyFrames().add(new KeyFrame(Duration.seconds(1), e -> {
             mCurrentAttempt.tick();
             setTimerText(mCurrentAttempt.getRemainingSeconds());
         }));
+        mTimeline.setOnFinished(event -> {
+            saveCurrentAttempt();
+            mApplause.play();
+            prepareAttempt(mCurrentAttempt.getKind() == AttemptKind.FOCUS ? AttemptKind.BREAK : AttemptKind.FOCUS);
+        });
+
+    }
+
+    private void saveCurrentAttempt() {
+        mCurrentAttempt.setMessage(message.getText());
+        mCurrentAttempt.save();
+    }
+
+    private void reset() {
+        clearAttemptStyles();
+        if(mTimeline != null && mTimeline.getStatus() == Animation.Status.RUNNING){
+            mTimeline.stop();
+        }
+    }
+
+    public void playTimer() {
+        container.getStyleClass().add("playing");
+        mTimeline.play();
+    }
+
+    public void pauseTimer(){
+        container.getStyleClass().remove("playing");
+        mTimeline.pause();
     }
 
     private void addAttemptStyle(AttemptKind kind) {
@@ -63,12 +97,27 @@ public class Home {
     }
 
     private void clearAttemptStyles() {
+        container.getStyleClass().remove("playing");
         for(AttemptKind kind : AttemptKind.values()){
             container.getStyleClass().remove(kind.toString().toLowerCase());
         }
     }
 
-    public void DEBUG(ActionEvent actionEvent) {
-        System.out.println("debugging...");
+    public void handleRestart(ActionEvent actionEvent) {
+        prepareAttempt(AttemptKind.FOCUS);
+        playTimer();
+    }
+
+    public void handlePlay(ActionEvent actionEvent) {
+        if(mCurrentAttempt == null){
+            handleRestart(actionEvent);
+        } else {
+            playTimer();
+        }
+
+    }
+
+    public void handlePause(ActionEvent actionEvent) {
+        pauseTimer();
     }
 }
